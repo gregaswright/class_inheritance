247620bc6118c8b75e01a9602bc88791f512f374
gregaswright <greg.a.s.wright@gmail.com>
2021-03-20 10:35:55 -0700

    blog 2 final

--- js/index.js
+++ js/index.js
@@ -10,15 +10,18 @@ Dog.prototype.fatten = function() {
     console.log(`The ${this.color} ${this.breed} was ${this.weight} pounds, but is now ${this.weight + 1} pounds. Woof!`)
 }
 
-let pug = new Dog('Pug', 'black', 5);
-let corgi = new Dog('Corgi', 'white', 10)
+// let pug = new Dog('Pug', 'black', 5);
+// let corgi = new Dog('Corgi', 'white', 10)
 
-console.log(pug.fatten(), corgi.fatten())
 
 
 
 function Pug() {
-
+    Dog.call(this, 'Pug', 'black', 5)
 }
 
 Pug.prototype = Object.create(Dog.prototype)
+
+let pug = new Pug()
+
+console.log(pug)
